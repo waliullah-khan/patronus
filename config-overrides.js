ebacac3b23a5a715d69987eea48977ba94242165
Wali Ullah Khan <114332722+waliullah-khan@users.noreply.github.com>
2025-06-11 20:17:56 -0400

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -11,7 +11,8 @@ module.exports = function override(config) {
         "os": require.resolve("os-browserify"),
         "url": require.resolve("url"),
         "path": require.resolve("path-browserify"),
-        "fs": false
+        "fs": false,
+        "webworker-threads": false
     })
     config.resolve.fallback = fallback;
     config.plugins = (config.plugins || []).concat([
